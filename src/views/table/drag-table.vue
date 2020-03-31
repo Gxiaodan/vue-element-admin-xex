ab1cc67b90e4d1b8dc58b5a756870b54e9ae017e
guan xiaodan <guanxd@sit.com.cn>
2020-03-31 14:35:33 +0800

    添加table列可拖拽

--- src/views/table/drag-table.vue
+++ src/views/table/drag-table.vue
@@ -2,15 +2,39 @@
   <div class="app-container">
     <!-- Note that row-key is necessary to get a correct row order. -->
     <el-table ref="dragTable" v-loading="listLoading" :data="list" row-key="id" border fit highlight-current-row style="width: 100%">
-      <el-table-column align="center" label="ID" width="65">
+
+      <el-table-column
+        v-for="(item, index) in cols"
+        :key="`col_${index}`"
+        :prop="dropCol[index].prop"
+        :label="item.label"
+      >
         <template slot-scope="{row}">
-          <span>{{ row.id }}</span>
+          <span v-if="dropCol[index].prop=='timestamp'">{{ row[dropCol[index].prop] | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+          <span v-else-if="dropCol[index].prop=='Drag'">
+            <svg-icon class="drag-handler" icon-class="drag" />
+          </span>
+          <span v-else-if="dropCol[index].prop== 'importance'">
+            <svg-icon v-for="n in + row.importance" :key="n" icon-class="star" class="icon-star" />
+          </span>
+          <span v-else-if="dropCol[index].prop== 'status'">
+            <el-tag :type="row.status | statusFilter">
+              {{ row.status }}
+            </el-tag>
+          </span>
+          <span v-else>{{ row[dropCol[index].prop] }}</span>
         </template>
       </el-table-column>
 
-      <el-table-column width="180px" align="center" label="Date">
+      <!-- <el-table-column align="center" label="ID" width="65" :prop="dropCol[getProp('ID')].prop">
         <template slot-scope="{row}">
-          <span>{{ row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+          <span>{{ row[dropCol[getProp('ID')].prop] }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="180px" align="center" label="Date" :prop="dropCol[getProp('Date')].prop">
+        <template slot-scope="{row}">
+          <span>{{ row[dropCol[getProp('Date')].prop] | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
         </template>
       </el-table-column>
 
@@ -50,7 +74,7 @@
         <template slot-scope="{}">
           <svg-icon class="drag-handler" icon-class="drag" />
         </template>
-      </el-table-column>
+      </el-table-column> -->
     </el-table>
     <div class="show-d">
       <el-tag>The default order :</el-tag> {{ oldList }}
@@ -87,6 +111,66 @@ export default {
         limit: 10
       },
       sortable: null,
+      dropCol: [
+        {
+          label: 'ID',
+          prop: 'id'
+        },
+        {
+          label: 'Date',
+          prop: 'timestamp'
+        },
+        {
+          label: 'Author',
+          prop: 'author'
+        },
+        {
+          label: 'Importance',
+          prop: 'importance'
+        },
+        {
+          label: 'Readings',
+          prop: 'pageviews'
+        },
+        {
+          label: 'Status',
+          prop: 'status'
+        },
+        {
+          label: 'Drag',
+          prop: 'Drag'
+        }
+      ],
+      cols: [
+        {
+          label: 'ID',
+          prop: 'id'
+        },
+        {
+          label: 'Date',
+          prop: 'timestamp'
+        },
+        {
+          label: 'Author',
+          prop: 'author'
+        },
+        {
+          label: 'Importance',
+          prop: 'importance'
+        },
+        {
+          label: 'Readings',
+          prop: 'pageviews'
+        },
+        {
+          label: 'Status',
+          prop: 'status'
+        },
+        {
+          label: 'Drag',
+          prop: 'Drag'
+        }
+      ],
       oldList: [],
       newList: []
     }
@@ -105,7 +189,14 @@ export default {
       this.newList = this.oldList.slice()
       this.$nextTick(() => {
         this.setSort()
+        this.columnDrop()
       })
+    },
+    getProp(val) {
+      const index = this.dropCol.findIndex(item => {
+        return item.label = val
+      })
+      return index
     },
     setSort() {
       const el = this.$refs.dragTable.$el.querySelectorAll('.el-table__body-wrapper > table > tbody')[0]
@@ -123,6 +214,19 @@ export default {
           // for show the changes, you can delete in you code
           const tempIndex = this.newList.splice(evt.oldIndex, 1)[0]
           this.newList.splice(evt.newIndex, 0, tempIndex)
+        }
+      })
+    },
+    // 列拖拽
+    columnDrop() {
+      const wrapperTr = document.querySelector('.el-table__header-wrapper tr')
+      this.sortable = Sortable.create(wrapperTr, {
+        animation: 180,
+        delay: 0,
+        onEnd: evt => {
+          const oldItem = this.dropCol[evt.oldIndex]
+          this.dropCol.splice(evt.oldIndex, 1)
+          this.dropCol.splice(evt.newIndex, 0, oldItem)
         }
       })
     }
